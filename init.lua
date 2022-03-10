ff0ccdef793bec06b5756fe64093fab7f15c4a59
bainchild <55368789+bainchild@users.noreply.github.com>
2022-03-09 18:41:27 -0600

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,20 +1,26 @@
 
 local prelude = [[
 ---@diagnostic disable: undefined-global, unused-local
-_R=_R or {a=0,b=0,c=0,d=0,ss='\0',ds='\0',fn=function()end,f={gt=false,lt=false,ge=false,le=false,eq=false,ne=false,err=false,syserr=false},sp=65536}
-_X=_X or {}
-_D=_D or {}
-_P=_P or {}
-_PD=_PD or {}
-_S,_ST=nil,nil
-_MMAP=_MMAP or {{a=0,b=0,set=function()end,get=function()end},{a=1,b=81920,set=function(p,x) _D[p]=x end,get=function(p) return _D[p] end}}
-_M=_M or function(p,x)
-for i=#_MMAP,1,-1 do
-    local v=_MMAP[i]
-    if p>=v.a and p<=v.b then
-    if x then v.set(p,x) else return v.get(p) end
+local definedAlready
+if _R and _X and _D and _P and _PD and _MMAP and _M then
+    definedAlready = true
+else
+    definedAlready = false
+    _R=_R or {a=0,b=0,c=0,d=0,ss='\0',ds='\0',fn=function()end,f={gt=false,lt=false,ge=false,le=false,eq=false,ne=false,err=false,syserr=false},sp=65536}
+    _X=_X or {}
+    _D=_D or {}
+    _P=_P or {}
+    _PD=_PD or {}
+    _S,_ST=nil,nil
+    _MMAP=_MMAP or {{a=0,b=0,set=function()end,get=function()end},{a=1,b=81920,set=function(p,x) _D[p]=x end,get=function(p) return _D[p] end}}
+    _M=_M or function(p,x)
+        for i=#_MMAP,1,-1 do
+            local v=_MMAP[i]
+            if p>=v.a and p<=v.b then
+            if x then v.set(p,x) else return v.get(p) end
+            end
+        end
     end
-end
 end
 local id=function(...) return ... end
 local asmcmp=function(a,b) return {lt=a<b,gt=a>b,le=a<=b,ge=a>=b,eq=a==b,ne=a~=b,err=false,syserr=false} end
@@ -24,23 +30,27 @@ local bit=bit or bit32
 local unpck=table and table.unpack or unpack or unpck
 local _Xargs,_Xnargs
 if not bit then
-bit={}
-bit.bnot=id
-bit.band=id
-bit.bor=id
-bit.bxor=id
-bit.lshift=id
-bit.rshift=id
-bit.rol=id
-bit.ror=id
+    bit={}
+    bit.bnot=id
+    bit.band=id
+    bit.bor=id
+    bit.bxor=id
+    bit.lshift=id
+    bit.rshift=id
+    bit.rol=id
+    bit.ror=id
 end
 ]]
 
 local port_std = [[
-_P[0x100]=function(a) _PD[0x100]=os.execute(a) end
-_P[0x101]=function(a) os.exit(a) end
-_P[0x102]=function(a) _PD[0x102]=os.getenv(a) end
-_P[0x103]=function() _PD[0x103]=os.time() end
+if not definedAlready then 
+    if os then
+        if os.execute then _P[0x100]=function(a) _PD[0x100]=os.execute(a) end end
+        if os.exit then _P[0x101]=function(a) os.exit(a) end end
+        if os.getenv then _P[0x102]=function(a) _PD[0x102]=os.getenv(a) end end
+        if os.time then _P[0x103]=function() _PD[0x103]=os.time() end end
+    end
+end
 ]]
 
 _ASM = {}
@@ -95,7 +105,7 @@ local compile = function(src, verbose, std, ports, mmap)
     elseif std then
         _ASM.std = require('include/std')
         prelude = prelude .. port_std
-        prelude = prelude .. '_MMAP[#_MMAP+1]={a=81921,b=81921,set=function(_,v) print(v) end,get=id}\n'
+        prelude = prelude .. 'if not definedAlready then _MMAP[#_MMAP+1]={a=81921,b=81921,set=function(_,v) print(v) end,get=id} end\n'
     end
 
     if _ASM.std then
